cb88383fe7686520325d11a309fe0bc6cd8982ff
muhammad-akif <muhammad-akif@outlook.com>
2021-08-31 22:27:10 +0500

    added server side models controllers routes ...

--- server/index.js
+++ server/index.js
@@ -1,4 +1,22 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose"
-import cors from "cors"
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import postRoutes from './routes/posts.js'
+
+const app = express()
+
+app.use('/post', postRoutes)
+
+app.use(bodyParser.json({limit:"30mb", extended: true}))
+app.use(bodyParser.urlencoded({limit:"30mb", extended: true}))
+app.use(cors())
+
+const CONNECTION_URL = "mongodb+srv://akif:16421@cluster0.6dv6g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL,{ useNewUrlParser: true,useUnifiedTopology: true})
+.then(()=> app.listen(PORT, ()=> console.log(`server running on port: ${PORT}`)))
+.catch((error)=>console.log(error.message))
+
+mongoose.set('useFindAndModify',false)
