d17ed75da6c6e88e3d8e6b26f10d6ff33f671ee6
muhammad-akif <muhammad-akif@outlook.com>
2021-08-31 23:33:31 +0500

    added client material-ui components in app.js and some basic file structures

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
-import { Container, AppBar, Typography, Grow, Grid } from 'material-ui/core'
-import memories from '../images/memories.png'
-import Posts from '../components/Posts/Posts'
-import Form from '../components/Form/Form'
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+import memories from './images/memories.png'
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
 function App() {
   return (
-    <Container>
+    <Container maxWidth="md">
       <AppBar position="static" color="inherit">
         <Typography variant="h2" align="center">Memories</Typography>
-        <img src={memories} alt="memories" height="60" />
+        <img src={memories} alt="memories" height="300" />
       </AppBar>
       <Grow in>
         <Container>
