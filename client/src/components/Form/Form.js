c911e7e5e4db19b9781025ce9b14f4bf67e8d67c
muhammad-akif <muhammad-akif@outlook.com>
2021-09-01 23:59:50 +0500

    Revert "added client APP."
    This reverts commit 89da2d9ef2ba1a55b0199044a93f88f819963a52.

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,8 +1,6 @@
 import React from 'react'
-// import makeStyles from './styles'
 
 const Form = () => {
-    // const classes = makeStyles()
     return (
         <>
             <h1>Form</h1>
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,14 +1,7 @@
 import React from 'react'
 import Post from './Post/Post'
-import makeStyles from './styles'
-import { useSelector } from 'react-redux'
 
 const Posts = () => {
-    const classes = makeStyles()
-    const posts = useSelector(state => state.posts)
-
-    console.log("posts ===> ",posts);
-
     return (
         <>
             <h1>Posts</h1>
