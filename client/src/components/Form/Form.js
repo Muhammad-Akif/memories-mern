1e09663d26db3539227441e30f73acc9baec2017
muhammad-akif <muhammad-akif@outlook.com>
2021-09-02 11:19:42 +0500

    working on client post Form

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import makeStyles from './styles'
+import { Paper, TextField, Typography, Button } from '@material-ui/core';
+import { typography } from '@material-ui/system';
 
 const Form = () => {
 
     const classes = makeStyles();
+    const [postData, setPostData] = useState({ creator:'', title: '', message= '', tags: '', selectedFile: '' })
     
     return (
-        <>
-            <h1>Form</h1>
-        </>
+        <Paper className={classes.paper}>
+            <form autoComplete="off" noValidate className={` ${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
+                <Typography variant="h6">Create</Typography>
+                <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={ (e) => setPostData({...postData, creator: e.target.value})} />
+            </form>
+        </Paper>
     )
 }
 
