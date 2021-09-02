4ca411dd10502f2b2cad2e930ad4b8879432e2f1
muhammad-akif <muhammad-akif@outlook.com>
2021-09-02 08:09:46 +0500

    added client Posts.js styles

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import Post from './Post/Post'
+import makeStyles from './styles'
 
 const Posts = () => {
+    
+    const classes = makeStyles();
+
     return (
         <>
             <h1>Posts</h1>
