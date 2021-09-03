16c5dd1a747a3a05a5e7152b672b6d218b48c4ff
muhammad-akif <muhammad-akif@outlook.com>
2021-09-03 17:11:08 +0500

    updatedPost action in client side

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -5,7 +5,7 @@ export const getPosts = () => async (dispatch) => {
         const { data } = await api.fetchPosts();
         dispatch({type: "FETCH_ALL", payload: data})
     } catch (error) {
-        console.log(error.message);
+        console.log(error);
     }
 }
 
@@ -14,16 +14,16 @@ export const createPost = (post) => async (dispatch) => {
         const { data } = await api.createPost(post);
         dispatch({type: "CREATE", payload: data})
     } catch (error) {
-        console.log(error.message);
+        console.log(error);
     }
 }
 
 export const updatePost = (currentId, post) => async (dispatch) => {
     try {
-        const { data } = await api.createPost(post);
-        dispatch({type: "CREATE", payload: data})
+        const { data } = await api.updatePost(currentId, post);
+        dispatch({type: "UPDATE", payload: data})
     } catch (error) {
-        console.log(error.message);
+        console.log(error);
     }
 }
 
