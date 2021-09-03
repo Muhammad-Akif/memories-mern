f15c1477af875b6f232d5b1f170bfabf26b13c07
muhammad-akif <muhammad-akif@outlook.com>
2021-09-03 14:43:57 +0500

    added post.js imports

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import makeStyles from './styles'
-
+import { Card, CardActions, CardContent, CardMedia, Button, Typography} from '@material-ui/core'
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import DeleteIcon from '@material-ui/icons/Delete';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
 const Post = ({ post }) => {
 
