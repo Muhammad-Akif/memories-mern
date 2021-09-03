96cae1257e3a445772e70c2db1406a32be63e426
muhammad-akif <muhammad-akif@outlook.com>
2021-09-03 14:40:44 +0500

    added posts.js UI

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import makeStyles from './styles'
 
-const Post = () => {
+
+const Post = ({ post }) => {
 
     const classes = makeStyles();
     
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Post from './Post/Post'
 import makeStyles from './styles'
+import { Grid, CircularProgress } from '@material-ui/core'
 import { useSelector } from 'react-redux'
 
 const Posts = () => {
@@ -10,10 +11,17 @@ const Posts = () => {
     console.log("posts ===> ",posts)
 
     return (
-        <>
-            <h1>Posts</h1>
-            <Post />
-        </>
+        !posts.length ? <CircularProgress/> : (
+            <Grid container className={classes.container} alignItems="stretch" spacing={3}>
+                {
+                    posts.map((post) => (
+                        <Grid key={post._id} item xs={12} sm={6}>
+                                <Post post={post} />
+                        </Grid>
+                    ))
+                }
+            </Grid>
+        )
     )
 }
 
