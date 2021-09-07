8399fed44295c78d3ff2946994b6323506a038a9
muhammad-akif <muhammad-akif@outlook.com>
2021-09-07 20:08:46 +0500

    added client delete api

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -5,10 +5,15 @@ import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
+import { useDispatch } from 'react-redux'
+import { deletePost } from '../../../actions/posts'
 
 const Post = ({ post, setCurrentId }) => {
 
     const classes = makeStyles();
+    const dispatch = useDispatch()
+
+    const deletePost = (id) => dispatch(deletePost(id))
 
     return (
         <Card className={classes.card}>
@@ -29,7 +34,7 @@ const Post = ({ post, setCurrentId }) => {
             </CardContent>
             <CardActions className={classes.cardActions}>
                 <Button size="small" color="primary" onClick={() => { }}><ThumbUpAltIcon fontSize="small" /> Like {post.likeCount} </Button>
-                <Button size="small" color="primary" onClick={() => { }}><DeleteIcon fontSize="small" /> Delete</Button>
+                <Button size="small" color="primary" onClick={() => { deletePost(post._id) }}><DeleteIcon fontSize="small" /> Delete</Button>
             </CardActions>
         </Card>
     )
