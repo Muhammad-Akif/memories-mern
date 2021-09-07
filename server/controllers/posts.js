bfd888a673ca4c7deb9eb334f5f082a74992d0fc
muhammad-akif <muhammad-akif@outlook.com>
2021-09-07 20:00:19 +0500

    added server Delete Controller

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,17 +1,17 @@
 import PostMessage from "../models/postMessage.js";
 import mongoose from 'mongoose';
 
-export const getposts = async (req,res)=>{
+export const getposts = async (req, res) => {
     try {
         const postMessage = await PostMessage.find()
 
         res.status(200).json(postMessage)
     } catch (error) {
-        res.status(404).json({message: error.message})
+        res.status(404).json({ message: error.message })
     }
 }
 
-export const createPost = async (req,res)=>{
+export const createPost = async (req, res) => {
     const post = req.body;
     const newPost = new PostMessage(post)
     try {
@@ -19,24 +19,22 @@ export const createPost = async (req,res)=>{
 
         res.status(201).json(newPost)
     } catch (error) {
-        res.status(409).json({message: error.message})
+        res.status(409).json({ message: error.message })
     }
 }
 
-export const updatePost = async (req,res)=>{
-    const {id: _id} = req.params;
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params;
     const post = req.body;
-    
+
     if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send(`No post with id: ${_id}`);
-    const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post, _id}, {new: true})
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true })
     res.json(updatedPost);
 }
 
-export const deletePost = (id) => {
-    try {
-        if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${_id}`);
-        PostMessage
-    } catch (error) {
-        
-    }
+export const deletePost = async (req, res) => {
+    const { id: _id } = req.params
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${_id}`);
+    await PostMessage.findByIdAndRemove(id)
+    res.json({ message: "Deleted Post Successfully!" })
 }
