8baccbaa9fe432a2c21596b45f125d4b44afe280
muhammad-akif <muhammad-akif@outlook.com>
2021-09-07 20:50:09 +0500

    completed server like Controller

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -39,7 +39,7 @@ export const deletePost = async (req, res) => {
     res.json({ message: "Deleted Post Successfully!" })
 }
 
-export const updatePost = async (req, res) => {
+export const likePost = async (req, res) => {
     const { id } = req.params;
 
     if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
