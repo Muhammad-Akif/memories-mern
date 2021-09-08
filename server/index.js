e2c9415a22f160480953cab070d3a48449bd5d99
muhammad-akif <muhammad-akif@outlook.com>
2021-09-08 12:05:41 +0500

    added server .env file

--- server/index.js
+++ server/index.js
@@ -13,6 +13,7 @@ app.use(cors())
 app.use('/post', postRoutes)
 
 const CONNECTION_URL = "mongodb+srv://akif:16421@cluster0.6dv6g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL,{ useNewUrlParser: true,useUnifiedTopology: true})
