4c91b018fad1d747b9a85b9a623c5ce45f94c302
muhammad-akif <muhammad-akif@outlook.com>
2021-09-10 15:57:08 +0500

    added routes and Home comp.

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import { Container, Grow, Grid } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
-
-import Posts from './components/Posts/Posts';
-import Form from './components/Form/Form';
-import { getPosts } from './actions/posts';
+import React from 'react';
+import { Container } from '@material-ui/core';
+import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 
 const App = () => {
-  const [currentId, setCurrentId] = useState(0);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getPosts());
-  }, [currentId, dispatch]);
-
   return (
-    <Container maxWidth="md">
-      <Navbar />
-      <Grow in>
-        <Container>
-          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-            <Grid item xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId} />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId} />
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
-    </Container>
+    <BrowserRouter>
+      <Container maxWidth="md">
+          <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home}/>
+        </Switch>
+      </Container>
+    </BrowserRouter>
   );
 };
 
