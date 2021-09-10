ad8527c0394c102bf0b0ca71d69aedfd2b5ac0dd
muhammad-akif <muhammad-akif@outlook.com>
2021-09-10 16:05:34 +0500

    added Auth component and completed routing

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container } from '@material-ui/core';
 import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
+import Auth from './components/Auth/Auth'
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 
@@ -12,6 +13,7 @@ const App = () => {
           <Navbar />
         <Switch>
           <Route path="/" exact component={Home}/>
+          <Route path="/auth" exact component={Auth}/>
         </Switch>
       </Container>
     </BrowserRouter>
---
+++ client/src/components/Auth/Auth.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Auth() {
+    const state = null;
+    return (
+        <div>
+            Auth component
+        </div>
+    )
+}
+
+export default Auth;
