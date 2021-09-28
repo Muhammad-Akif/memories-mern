df1415f06c8df006c1fe8bfe347b99ed1fc72edc
muhammad-akif <muhammadakif2917@gmail.com>
2021-09-28 23:25:47 +0500

    added styling to navbar and app background

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,7 +1,8 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { AppBar, Typography, Toolbar, Avatar, Button } from '@material-ui/core';
 import { Link } from 'react-router-dom';
-import memories from '../../images/memories.png';
+import logo from '../../images/logo.png';
+import logoText from '../../images/logoText.png';
 import useStyles from './styles';
 import { useLocation, useHistory } from 'react-router-dom'
 import { useDispatch } from 'react-redux';
@@ -15,29 +16,29 @@ function Navbar() {
     const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")))
 
     const logout = () => {
-        dispatch({type: "LOGOUT"})
+        dispatch({ type: "LOGOUT" })
         history.push("/");
         setUser(null)
     }
-    
-    useEffect(()=>{
+
+    useEffect(() => {
         const token = user?.token;
-        if(token) {
+        if (token) {
             const decodedToken = decode(token)
 
-            if (decodedToken.exp * 1000 < new Date().getTime()){
+            if (decodedToken.exp * 1000 < new Date().getTime()) {
                 logout();
-            } 
+            }
         }
         setUser(JSON.parse(localStorage.getItem('profile')));
-    },[location]);
-    
+    }, [location]);
+
     return (
         <AppBar className={classes.appBar} position="static" color="inherit">
-            <div className={classes.brandContainer}>
-                <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">Memories</Typography>
-                <img className={classes.image} src={memories} alt="icon" height="60" />
-            </div>
+            <Link to="/" className={classes.brandContainer}>
+                <img src={logoText} alt="icon" height="45px" />
+                <img className={classes.image} src={logo} alt="icon" height="40px" />
+            </Link>
             <Toolbar className={classes.toolbar}>
                 {user ? (
                     <div className={classes.profile}>
