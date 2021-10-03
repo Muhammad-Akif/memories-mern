d3a98412e990feec67dfdd5b6274a6be235feb50
muhammad-akif <muhammadakif2917@gmail.com>
2021-10-03 12:12:01 +0500

    added post reducer

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,13 +1,18 @@
-import { START_LOADING, END_LOADING, FETCH_ALL, FETCH_BY_SEARCH, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
+import { START_LOADING, END_LOADING, FETCH_POST, FETCH_ALL, FETCH_BY_SEARCH, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
 
-const reducer = ( state = { isLoading: true, posts: [] }, action) => {
+const reducer = (state = { isLoading: true, posts: [] }, action) => {
   switch (action.type) {
     case START_LOADING:
-      return {...state, isLoading: true};
+      return { ...state, isLoading: true };
     case END_LOADING:
-      return {...state, isLoading: false};
+      return { ...state, isLoading: false };
+    case FETCH_POST:
+      return {
+        ...state,
+        post: action.payload.data,
+      };
     case FETCH_ALL:
-      return { 
+      return {
         ...state,
         posts: action.payload.data,
         currentPage: action.payload.currentPage,
@@ -19,13 +24,13 @@ const reducer = ( state = { isLoading: true, posts: [] }, action) => {
         posts: action.payload
       };
     case LIKE:
-      return { ...state, posts: state.posts.map((post) => (post._id === action.payload._id ? action.payload : post))};
+      return { ...state, posts: state.posts.map((post) => (post._id === action.payload._id ? action.payload : post)) };
     case CREATE:
-      return { ...state, posts: [...state.posts, action.payload]};
+      return { ...state, posts: [...state.posts, action.payload] };
     case UPDATE:
-      return { ...state, posts: state.posts.map((post) => (post._id === action.payload._id ? action.payload : post))};
+      return { ...state, posts: state.posts.map((post) => (post._id === action.payload._id ? action.payload : post)) };
     case DELETE:
-      return { ...state, posts: state.posts.filter((post) => post._id !== action.payload)};
+      return { ...state, posts: state.posts.filter((post) => post._id !== action.payload) };
     default:
       return state
   }
