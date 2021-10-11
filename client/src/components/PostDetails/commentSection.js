1278757c38075432bf755edf01516eb11a133082
muhammad-akif <muhammadakif2917@gmail.com>
2021-10-11 17:17:15 +0500

    working on comments Section

--- client/src/components/PostDetails/commentSection.js
+++ client/src/components/PostDetails/commentSection.js
@@ -8,18 +8,37 @@ const CommentSection = ({post}) => {
     const classes = useStyles();
     const dispatch = useDispatch();
     const [comments, setComments] = useState([1,2,3,4,5]) 
+    const [comment, setComment] = useState() 
+    const handleClick = () => {
+
+    }
 
     return (
         <div className={classes.commentsOuterContainer}>
             <div className={classes.commentsInnerContainer}>
                 <Typography gutterBottom variant="h6">Comments</Typography>
                 {
-                    comments.map((c, i) => {
-                        <Typography>
-                            Comments are {i}
+                    comments.map((c, i) => (
+                        <Typography gutterBottom variant="subtitle1">
+                            Comments {c} 
                         </Typography>
-                    })
+                    ))
                 }
+            </div>
+            <div style={{ width: "70%" }}>
+                <Typography gutterBottom variant="h6">Write a Comment</Typography>
+                <TextField
+                    fullWidth 
+                    variant="outlined"
+                    rows={4}
+                    label="Comment"
+                    multiline
+                    value={comment}
+                    onChange={(e) => setComment(e.target.value)}
+                />
+                <Button style={{ marginTop: '10px'}} color="primary" fullwidth disabled={!comment} variant="contained"onClick={handleClick}>
+                    Comment
+                </Button>
             </div>
         </div>
     )
